569ab278fae295a1b0735c6f8bf4c1c9391f3aab
Simon Bäumer <bs@shopware.com>
2015-08-05 13:52:37 +0200

    implemented document service

---
+++ engine/Shopware/Components/Doucment/Document.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Shopware\Components\Document;
+
+class Document
+{
+    public function __construct()
+    {
+        echo "<pre>";
+        print_r("test");
+        echo "<pre>";
+        exit();
+    }
+}
